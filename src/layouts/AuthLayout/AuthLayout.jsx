410375968c358bc4efe89cda3b84c91717f71e59
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-01-31 11:44:08 +0100

    added auth page desgin code

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
-import AuthGuard from "../../shared/guards/AuthGuard";
+import dotsImg from "../../assets/images/dots.svg";
+import MigrosLogo from "../../assets/logos/migros_logo.svg";
+import restoranIllustration from "../../assets/images/restaurant_illustration.svg";
 
 const AuthLayout = () => {
   return (
-      <div className="auth_layout">
-        <Outlet />
+    <div className="auth_layout">
+      <div className="left_container">
+        <div className="top">
+          <img src={MigrosLogo} alt="" />
+          <img src={dotsImg} alt="" className="dots-1"/>
+        </div>
+        <div className="middle">
+            <span className="main-heading">
+            Migros Restoran Panel’e<br/> Hoş Geldiniz
+            </span>
+            <img src={restoranIllustration} alt="" />
+        </div>
+        <div className="bottom">
+          <img src={dotsImg} alt="" className="dots-2"/>
+        </div>
       </div>
+      <Outlet />
+    </div>
   );
 };
 
