7224f2aea7cf8aeeb301bab47db1787ced5bd3fb
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-01-31 14:48:22 +0100

    finished authLayout

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -6,22 +6,25 @@ import restoranIllustration from "../../assets/images/restaurant_illustration.sv
 const AuthLayout = () => {
   return (
     <div className="auth_layout">
-      <div className="left_container">
+      <div className="fixed_container">
         <div className="top">
           <img src={MigrosLogo} alt="" />
-          <img src={dotsImg} alt="" className="dots-1"/>
+          <img src={dotsImg} alt="" className="dots-1" />
         </div>
         <div className="middle">
-            <span className="main-heading">
-            Migros Restoran Panel’e<br/> Hoş Geldiniz
-            </span>
-            <img src={restoranIllustration} alt="" />
+          <span className="main-heading">
+            Migros Restoran Panel’e
+            <br /> Hoş Geldiniz
+          </span>
+          <img src={restoranIllustration} alt="" />
         </div>
         <div className="bottom">
-          <img src={dotsImg} alt="" className="dots-2"/>
+          <img src={dotsImg} alt="" className="dots-2" />
         </div>
       </div>
-      <Outlet />
+      <div className="outlet_container">
+        <Outlet />
+      </div>
     </div>
   );
 };
