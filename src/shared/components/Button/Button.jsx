f3b86e22f31800a1202fb3b5151a11f3dc6313cc
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-02-03 18:36:54 +0100

    disabled state

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -1,9 +1,10 @@
-function Button({ label, onClick, isDisabled, type }) {
+function Button({ label, onClick, disabled, type }) {
   return (
     <button
-      className={isDisabled ? "button_container disabled" : "button_container"}
+      className={disabled ? "button_container disabled" : "button_container"}
       onClick={onClick}
       type={type}
+      disabled={disabled}
     >
       <span className="label">{label || "button"}</span>
     </button>
