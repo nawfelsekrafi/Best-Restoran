1e7b009e185ce6d888ae6f5ea15af831b9c51218
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-02-05 23:20:46 +0100

    edited not found page

--- src/shared/components/NotFound/NotFound.jsx
+++ src/shared/components/NotFound/NotFound.jsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import { ReactComponent as NotFoundImg } from "../../../assets/images/Not_found.svg";
+import { ReactComponent as NotFoundImg } from "../../../assets/images/not-found-1.svg";
+import Button from "../Button";
 
 function NotFound() {
   return (
     <div className="not__found__container">
+      <span className="title">
+        Oops. The page you were looking for doesn’t exist
+      </span>
+      <span className="description">
+        We are sorry that the page you are looking for could not be found. We
+        recommend that you return to the main page.
+      </span>
+
+      <Link to={"/"} className="button">
+        <Button label="Back to Home Page" />
+      </Link>
       <NotFoundImg />
-      <p>Oops. The page you were looking for doesn’t exist</p>
-      <Link to={"/"}>Take me back to the home page</Link>
     </div>
   );
 }
