34fd42f4bac29f6bbb337de35548a034dee18689
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-02-05 23:26:29 +0100

    imported switch component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,19 @@
+import Switch from "../../shared/components/Switch";
+
 const Home = () => {
-  return <div className="home_page">Home Page</div>;
+  return (
+    <div className="home_page">
+      <Switch
+        // label="active"
+        value={true}
+        onChange={(value) => console.log(value)}
+      />
+      {/* <div className="hero_section">
+        <Carosel images={[img1, img2, img]} />
+      </div> */}
+      Home Page
+    </div>
+  );
 };
 
 export default Home;
