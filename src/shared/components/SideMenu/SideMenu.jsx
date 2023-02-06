7e9f27322084dc61d4b52e8da14a0ac9871a6724
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-02-06 23:11:36 +0100

    edited sideMenu

--- src/shared/components/SideMenu/SideMenu.jsx
+++ src/shared/components/SideMenu/SideMenu.jsx
@@ -1,51 +1,86 @@
-import { NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { changePage } from "../../../data/slices/pagesSlice";
+import { closeSidebar } from "../../../data/slices/settingsSlice";
 import useWindowSize from "../../../hooks/useWindowDimensions";
 import icon3 from "../../../assets/icons/sidebar/angle-down.svg";
 import icon4 from "../../../assets/icons/sidebar/angle-up.svg";
 import placeholderIcon from "../../../assets/icons/sidebar/angle-up.svg";
-import { useState } from "react";
 
 function SideMenu({ title, icon1, icon2, link, isToggable, childrens, nb }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const linkRef = useRef(null);
   const { width } = useWindowSize();
+  const childrenRef = useRef(null);
   const [showChildren, setShowChildren] = useState(false);
-  const toggleShowChildren = () => {
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    setRendered(true);
+  }, []);
+  useEffect(() => {
+    if (rendered) {
+      if (childrens && childrens.length == 3) {
+        childrenRef.current.classList.toggle("children--3--isActive");
+      }
+      if (childrens && childrens.length == 5) {
+        childrenRef.current.classList.toggle("children--5--isActive");
+      }
+    }
+  }, [showChildren]);
+
+  const toggleShowChildren = (e, link) => {
+    const nodes = document.getElementsByClassName("sideMenu_container");
+    [...nodes].forEach((e) => e.classList.remove("sideMenu_container--Active"));
     setShowChildren((prv) => {
+      if (!isToggable) {
+        navigate("/" + link);
+        dispatch(changePage(title));
+        linkRef.current.classList.add("sideMenu_container--Active");
+        setTimeout(() => {
+          if (width < 700) {
+            dispatch(closeSidebar());
+          }
+        }, 200);
+      } else {
+        e.preventDefault();
+        linkRef.current.classList.remove("sideMenu_container--Active");
+      }
       return !prv;
     });
   };
   return (
     <>
       {" "}
-      <NavLink
-        style={width < 1101 ? { width: "fit-content" } : {}}
-        className={({ isActive }) =>
-          isActive
-            ? "sideMenu_container sideMenu_container--Active"
-            : "sideMenu_container"
-        }
-        to={"/" + link}
-        onClick={toggleShowChildren}
-        end
+      <div
+        ref={linkRef}
+        className="sideMenu_container"
+        onClick={(e) => toggleShowChildren(e, link)}
       >
         {icon1 && <img src={icon1} alt="" className="icon1" />}
         {icon2 && <img src={icon2} alt="" className="icon2" />}
         {!icon1 && !icon2 && (
           <img src={placeholderIcon} alt="" className="icon5" />
         )}
-        {width > 1101 && <span className="label">{title}</span>}
-        {width > 1101 && isToggable && (
-          <img src={icon3} alt="" className="icon3" />
-        )}
-        {width > 1101 && isToggable && (
-          <img src={icon4} alt="" className="icon4" />
-        )}
-        {width > 1101 && nb && (
-          <span className="number">{nb < 100 ? nb : "+99"}</span>
-        )}
-      </NavLink>
-      {showChildren &&
-        childrens &&
-        childrens.map((el) => <SideMenu {...el} key={el.id} />)}
+        <>
+          <span className="label">{title}</span>
+          {isToggable && (
+            <>
+              <img src={icon3} alt="" className="icon3" />
+              <img src={icon4} alt="" className="icon4" />
+            </>
+          )}
+          {nb && <span className="number">{nb < 100 ? nb : "+99"}</span>}
+        </>
+      </div>
+      {childrens && (
+        <div className="children" ref={childrenRef}>
+          {childrens.map((el) => (
+            <SideMenu {...el} key={el.id} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
