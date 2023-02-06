f1377092f9617aef903c811f489840ac510c59b0
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2023-02-06 23:51:35 +0100

    fixed navbar jsx

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -1,8 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { openSidebar } from "../../data/slices/settingsSlice";
 import NavbarNotificationsBox from "../../shared/components/NavbarNotificationsBox";
+import RestoranLogo from "../../assets/logos/restoran_logo.svg";
+import sidebarIcon from "../../assets/icons/menu.svg";
+
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const { currentPage } = useSelector((state) => state.pages);
+
   return (
     <div className="navbar_container">
-      <span className="page_name">Ana Sayfa</span>
+      <div
+        className="sidebarIcon"
+        onClick={() => {
+          dispatch(openSidebar());
+        }}
+      >
+        <img src={sidebarIcon} alt="" />
+      </div>
+
+      <span className="page_name">{currentPage}</span>
+      <img src={RestoranLogo} alt="" className="restoran_logo" />
       <div className="notification_admin">
         <NavbarNotificationsBox username="Murat G." role="Restoran Yetkilisi" />
       </div>
